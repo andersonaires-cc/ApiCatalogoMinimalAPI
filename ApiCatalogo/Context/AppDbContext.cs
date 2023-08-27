c14f942da82c82fb71b42e6723ef7c5931238611
anderson <andersonairesspp@hotmail.com>
2023-08-27 11:47:06 -0300

    Configuring properties and relationships

--- ApiCatalogo/Context/AppDbContext.cs
+++ ApiCatalogo/Context/AppDbContext.cs
@@ -17,7 +17,29 @@ namespace ApiCatalogo.Context
 
         protected override void OnModelCreating(ModelBuilder mb)
         {
+            //categoria
+            mb.Entity<Categoria>().HasKey(c => c.CategoriaId);
+
+            mb.Entity<Categoria>().Property(c => c.Nome).HasMaxLength(100)
+                                                        .IsRequired();
             
+            mb.Entity<Categoria>().Property(c => c.Descricao).HasMaxLength(100)
+                                                             .IsRequired();
+            //produto
+            mb.Entity<Produto>().HasKey(c => c.ProdutoId);
+            mb.Entity<Produto>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
+            mb.Entity<Produto>().Property(c => c.Descricao).HasMaxLength(150);
+            mb.Entity<Produto>().Property(c => c.Imagem).HasMaxLength(100);
+            mb.Entity<Produto>().Property(c => c.Preco).HasPrecision(14,2);
+
+            //relacionamento
+            //Produto tem uma relação com 1 categoria e vários produtos.
+            mb.Entity<Produto>()
+                .HasOne<Categoria>(c => c.Categoria)
+                .WithMany(p => p.Produtos)
+                .HasForeignKey(c => c.CategoriaId);
+
+
         }
 
     }
