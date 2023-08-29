67063cb335b6b381fb8c541e4da0fa800acff36b
anderson <andersonairesspp@hotmail.com>
2023-08-28 22:04:31 -0300

    Endpoint MapPost

--- ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/Models/Categoria.cs
@@ -1,10 +1,13 @@
-﻿namespace ApiCatalogo.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ApiCatalogo.Models
 {
     public class Categoria
     {
         public int CategoriaId { get; set; }
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
+        [JsonIgnore]
         public ICollection<Produto>? Produtos { get; set; }
     }
 }
--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -1,4 +1,5 @@
 using ApiCatalogo.Context;
+using ApiCatalogo.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,7 +20,14 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Catálogo de Produtos - 2022");
 
+app.MapPost("/categorias", async (Categoria categoria, AppDbContext db)
+   =>
+{
+    db.categorias.Add(categoria);
+    await db.SaveChangesAsync();
 
+    return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
