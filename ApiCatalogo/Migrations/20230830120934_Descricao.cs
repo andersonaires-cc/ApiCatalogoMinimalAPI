965a9088faf3bd77aee832e83ccc2ecf3a49efbd
anderson <andersonairesspp@hotmail.com>
2023-08-30 09:41:07 -0300

    Endpoints Produtos

---
+++ ApiCatalogo/Migrations/20230830120934_Descricao.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    public partial class Descricao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_categorias_CategoriaId",
+                table: "Produtos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_categorias",
+                table: "categorias");
+
+            migrationBuilder.RenameTable(
+                name: "categorias",
+                newName: "Categorias");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "varchar(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 150,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categorias",
+                table: "Categorias",
+                column: "CategoriaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Categorias_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "CategoriaId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Categorias_CategoriaId",
+                table: "Produtos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categorias",
+                table: "Categorias");
+
+            migrationBuilder.RenameTable(
+                name: "Categorias",
+                newName: "categorias");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "int",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)",
+                oldMaxLength: 150,
+                oldNullable: true)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_categorias",
+                table: "categorias",
+                column: "CategoriaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_categorias_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId",
+                principalTable: "categorias",
+                principalColumn: "CategoriaId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
